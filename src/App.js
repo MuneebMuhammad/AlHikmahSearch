5843c218e940bfa36fca88c92c959864bd122d19
Muneeb Muhammad <muneebkattak57@gmail.com>
2023-08-31 11:01:18 +0500

    name changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Muneeb Edit <code>src/App.js</code> and save to reload.
+         Al Hikmah Search Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
